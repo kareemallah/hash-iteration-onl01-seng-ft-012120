bbed40c6feea233e6a6ccc02c915794ab10844fc
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-27 16:30:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,3 +12,7 @@ def happy_birthday(birthday_kids)
 end
 
 
+def age_appropriate_birthday(birthday_kids <= 12)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
